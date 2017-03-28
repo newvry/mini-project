adba481478144a75a92afe8f1fd71037e4b9dd70
LYC <newvry@hotmail.com>
2017-03-28 18:14:39 +0800

    create about the web

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    collection do
+      get :profile
+    end
+  end
 
   resources :topics do
     member do
       post :comments
+    end
+    collection do
+      get :about
     end
   end
   root "topics#index"
