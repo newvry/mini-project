15a2c8a1c38d05a849043fdbc0821638a59237f2
LYC <newvry@hotmail.com>
2017-03-28 18:13:10 +0800

    user can create nickname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327065833) do
+ActiveRecord::Schema.define(version: 20170328081045) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -37,9 +37,11 @@ ActiveRecord::Schema.define(version: 20170327065833) do
     t.string   "name"
     t.date     "date"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "user_id"
+    t.datetime "comment_last"
+    t.integer  "comments_count", default: 0
     t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
@@ -56,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170327065833) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
