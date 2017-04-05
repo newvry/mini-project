c7171b0f354987cb5447da6f50f457ba086f02f0
LYC <newvry@hotmail.com>
2017-04-05 10:32:42 +0800

    user can see who comment in the topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -9,4 +9,16 @@ class Topic < ApplicationRecord
   has_one :photo, dependent: :destroy
   accepts_nested_attributes_for :photo
 
+
+  def comment_user
+    comment_user = []
+    self.comments.each do |f|
+    #self是指show的@topic，哪個topic呼叫，self就是指呼叫的那個topic，所以這裡不能打@topic，
+    #因為我們在show裡，已經要用@topic.comment_user來呼叫這個方法，@topic就是指self
+      comment_user << f.user.first_email
+    end
+
+    return comment_user.uniq
+  end
+
 end
